966d9bac1c8668693fb7a869b3e9ff48dae0402f
Oleh Kutafin <kutafin.o.v@gmail.com>
2024-08-26 16:38:18 -0700

    add projects for homework

---
+++ homework_12/tests/LibraryV3.NUnit.Tests.Api/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace LibraryV3.NUnit.Tests.Api;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
---
+++ homework_12/tests/LibraryV3.xUnit.TestsApi/UnitTest1.cs
@@ -0,0 +1,9 @@
+namespace LibraryV3.xUnit.TestsApi;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
