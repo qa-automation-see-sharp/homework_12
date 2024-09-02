813fb1026ffbc0ec2d51c646cb0380e75d06efb9
OlenaMaliuhina2 <olenamaliuhina22@gmail.com>
2024-09-02 20:41:14 +0200

    finish homework

--- tests/LibraryV3.NUnit.Tests.Api/UnitTest1.cs
+++ tests/LibraryV3.NUnit.Tests.Api/UnitTest1.cs
@@ -1,15 +1,15 @@
 namespace LibraryV3.NUnit.Tests.Api;
 
-public class Tests
-{
-    [SetUp]
-    public void Setup()
-    {
-    }
+//public class Tests
+//{
+//    [SetUp]
+//    public void Setup()
+//    {
+//    }
 
-    [Test]
-    public void Test1()
-    {
-        Assert.Pass();
-    }
-}
+//    [Test]
+//    public void Test1()
+//    {
+//        Assert.Pass();
+//    }
+//}
--- tests/LibraryV3.xUnit.Tests.Api/UnitTest1.cs
+++ tests/LibraryV3.xUnit.Tests.Api/UnitTest1.cs
@@ -1,9 +1,9 @@
-namespace LibraryV3.xUnit.Tests.Api;
+//namespace LibraryV3.xUnit.Tests.Api;
 
-public class UnitTest1
-{
-    [Fact]
-    public void Test1()
-    {
-    }
-}
+//public class UnitTest1
+//{
+//    [Fact]
+//    public void Test1()
+//    {
+//    }
+//}
