7c3f0350df94c980a53754cea4ac88b1841d9863
Liudmyla <savinska.l.y@gmail.com>
2024-09-23 12:42:55 +0200

    XUnit

---
+++ tests/LibraryV3.xUnit.Tests.Api/TestDataCollection.cs
@@ -0,0 +1,7 @@
+namespace LibraryV3.xUnit.Tests.Api;
+
+[CollectionDefinition("LibraryV3 Collection")]
+public class TestDataCollection //: ICollectionFixture<WebApplicationFactory<IApiMarker>>
+{
+    
+}
